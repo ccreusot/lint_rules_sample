cb7b59ff2d8d3d1977973287ce33dcd309667b63
Cédric <cedric.creusot@gmail.com>
2019-06-24 22:33:00 +0200

    add first detector that doing nothing and fix problems from exported build.gradle

--- customlintrules/src/main/kotlin/com/ccreusot/customlintrules/CustomRulesIssueRegistry.kt
+++ customlintrules/src/main/kotlin/com/ccreusot/customlintrules/CustomRulesIssueRegistry.kt
@@ -1,3 +1,25 @@
 package com.ccreusot.customlintrules
 
-class CustomRulesIssueRegistry
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.*
+import java.util.*
+
+class CustomRulesIssueRegistry : IssueRegistry() {
+    override val issues: List<Issue> = listOf(
+        Issue.create(
+            "JudgeDredd",
+            "You can't use Judge()",
+             "You can't construct a Judge by your self, only a Judge can instance it self. (Judge.newInstance())",
+            Category.CORRECTNESS,
+            5,
+            Severity.FATAL,
+            Implementation(
+                JudgeConstructDetector::class.java,
+                EnumSet.of(Scope.JAVA_FILE, Scope.TEST_SOURCES)
+            )
+        )
+    )
+
+    override val api: Int
+        get() = CURRENT_API
+}
---
+++ customlintrules/src/main/kotlin/com/ccreusot/customlintrules/JudgeConstructDetector.kt
@@ -0,0 +1,7 @@
+package com.ccreusot.customlintrules
+
+import com.android.tools.lint.detector.api.Detector
+
+class JudgeConstructDetector : Detector(), Detector.UastScanner {
+
+}
